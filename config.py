0e7b2af31d20f27e317a7351d459a85d718f6d23
Rokstar890 <144723163+Rokstar890@users.noreply.github.com>
2024-01-31 10:18:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
 #Website Credentials
-API_KEY = environ.get('API_KEY', '5f82d8793633efb1268e8a4d3e7b4911e217d20a')
-API_URL = environ.get('API_URL', 'https://pandaznetwork.com/api')
-WEB_NAME = environ.get('WEB_NAME', 'PandazNetwork')
+API_KEY = environ.get('API_KEY', '')
+API_URL = environ.get('API_URL', 'https://linksmoney.in/api')
+WEB_NAME = environ.get('WEB_NAME', 'linksmoney.in')
 
 #Optional
-SUPPORT_GROUP = environ.get('SUPPORT_GROUP', 'https://t.me/pandaztalks')
-UPDATES_CHANNEL = environ.get('UPDATES_CHANNEL', 'https://t.me/pandaznetwork')
+SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '')
+UPDATES_CHANNEL = environ.get('UPDATES_CHANNEL', '')
