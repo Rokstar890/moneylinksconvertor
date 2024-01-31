017cef08883c979abfb7bf21a1d115752663b6cb
Rokstar890 <144723163+Rokstar890@users.noreply.github.com>
2024-01-31 10:21:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
 #Website Credentials
-API_KEY = environ.get('API_KEY', '')
+API_KEY = environ.get('API_KEY', 'c7951680b0ff1e52743c10eae2f4de6a6e1175b1')
 API_URL = environ.get('API_URL', 'https://linksmoney.in/api')
 WEB_NAME = environ.get('WEB_NAME', 'linksmoney.in')
 
