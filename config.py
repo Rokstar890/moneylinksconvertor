3f124fb6d0fe22d644ea5fe03b289d85958da410
Rokstar890 <144723163+Rokstar890@users.noreply.github.com>
2024-01-31 10:24:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import environ
 
 #TG Credentials
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+API_ID = int(environ.get('API_ID', '24003416'))
+API_HASH = environ.get('API_HASH', '013d9b3fe388308ff1272d33d1e40e2b')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6858627899:AAEtsmzG_9kRpwXqkULMXn56Bn5K5fTp4fU')
 
 #Website Credentials
 API_KEY = environ.get('API_KEY', 'c7951680b0ff1e52743c10eae2f4de6a6e1175b1')
